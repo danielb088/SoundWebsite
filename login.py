b6e30bed6b8e9816343bf9dcb07bd109d86eadfe
danielb088 <danielbaridk@gmail.com>
2025-09-17 15:59:51 +0300

    added login page

---
+++ login.py
@@ -0,0 +1,15 @@
+from nicegui import ui
+
+with ui.row().classes("w-full justify-center gap-5"):
+    ui.label("Log in").style('font-family: Comic Sans MS; font-size: 37px; font-weight: bold;')
+with ui.row().classes("w-full justify-center gap-5"):
+    with ui.column():
+            ui.input(placeholder="Enter your first name")
+            ui.input(placeholder="Enter your last name")
+
+with ui.row().classes("w-full justify-center gap-5"):
+    ui.button('Log in')
+    ui.button('Sign up', on_click=lambda: ui.notify('You are now signed up!'))
+with ui.row().classes("w-full justify-center gap-5"):
+    ui.button('Forgot password', on_click=lambda: ui.notify('You are now signed up!'))
+ui.run()
