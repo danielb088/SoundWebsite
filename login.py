41c90f8b4a1c7674334f2875d28b56ad541f668b
danielb088 <danielbaridk@gmail.com>
2025-09-19 00:57:01 +0300

    add button

--- login.py
+++ login.py
@@ -12,5 +12,7 @@ with ui.row().classes("w-full justify-center gap-5"):
     ui.button('Sign up', on_click=lambda: ui.notify('You are now signed up!'))
 with ui.row().classes("w-full justify-center gap-5"):
     ui.button('Forgot password', on_click=lambda: ui.notify('You are now signed up!'))
+# with ui.div(style="position: fixed; bottom: 0; left: 0;"):
+# with ui.dropdown_button(auto_close=True):
 ui.slider(min=0, max=100, value=50)
 ui.run()
