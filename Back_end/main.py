1ff4383f282a10da34ec83525e6ccee20095ceb9
danielb088 <danielbaridk@gmail.com>
2025-11-16 12:31:17 +0200

    add router to main

--- Back_end/main.py
+++ Back_end/main.py
@@ -9,7 +9,7 @@ import uvicorn
 from api.user import router as user_router
 from api.songs import router as songs_router
 from api.listens import router as listens_router
-from api.following import router as following
+from api.following import router as following_router
 
 
 if __name__ == "__main__":
@@ -18,3 +18,6 @@ else:
     init_db([User,Following,Listens,Songs])
     app = FastAPI()
     app.include_router(user_router)
+    app.include_router(songs_router)
+    app.include_router(listens_router)
+    app.include_router(following_router)
