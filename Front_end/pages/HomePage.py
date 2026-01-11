b1b669b3046ad9ac48502f72d063e3a5e49443f5
danielb088 <danielbaridk@gmail.com>
2026-01-11 12:52:01 +0200

    admin cookies

--- Front_end/pages/HomePage.py
+++ Front_end/pages/HomePage.py
@@ -31,7 +31,8 @@ def HomePage():
     ui.colors(primary='#ccf71f')
     with ui.card().style('width: 100%'):
         with ui.row().classes("w-full justify-center gap-5"):
-            ui.button("Statistics", on_click=Statistics_click)
+            if is_admin:
+                ui.button("Statistics", on_click=Statistics_click)
             ui.input(placeholder="Search:")
             ui.label("Sound").style('font-family: Comic Sans MS; font-size: 30px; font-weight: bold;')
     with ui.row().classes("w-full justify-center gap-5"):
